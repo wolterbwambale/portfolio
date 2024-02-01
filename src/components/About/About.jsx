801211431cd7058b20306a6ce58604f23c51c073
Bwambale Benny Wolter <benplaan1@gmail.com>
2024-02-02 00:30:46 +0300

    add about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './About.module.css';
+import { getImageUrl } from '../../utils';
+
+const About = () => {
+  return (
+    <section className={styles.container} id="about">
+        <h1 className={styles.title}>About</h1>
+        <div className={styles.content}> 
+            <img src={getImageUrl("about/hacker.png")} alt="me seated with computer" className={styles.aboutMaainImg} />
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursor.png")} alt="pointer" className={styles.aboutImage} />
+                <div className={styles.aboutItemText}>
+                <h3>Frontend Developer</h3>
+                <p>I'm frontend with experience of building responsive and optimize sites</p>
+                </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/server.png")} alt="server"className={styles.aboutImage} />
+                <div className={styles.aboutItemText}>
+                <h3>Backend Developer</h3>
+                <p>I have Experience of developing optimised Backend systems and API's</p>
+                </div>
+                </li>
+
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursor.png")} alt="pointer"className={styles.aboutImage} />
+                <div className={styles.aboutItemText}>
+                <h3>UI Design</h3>
+                <p>I have design multiple  landing pages and i have created  systems as well</p>
+                </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+  )
+}
+
+export default About
