dc56d4629cc4dc452d38b4dab12b2ea51e78017a
Bwambale Benny Wolter <benplaan1@gmail.com>
2024-02-04 15:08:08 +0300

    add experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import  styele from './Experience.module.css'
+import skills from '../../data/skills'
+import { getImageUrl } from '../../utils'
 
 const Experience = () => {
   return (
-    <div >Experience</div>
+    <section id="experience">
+        <h1>Experience</h1>
+        <div>
+            <div>{skills.map((skill, id) =>{
+                return(
+                   <div key={id}>
+                    <div>
+                    <img src={getImageUrl(skill.imgscr)} alt={skill.title} />
+                    </div> 
+                    <p>{skill.title}</p>
+                  </div>
+                )
+            })}</div>
+        </div>
+
+    </section>
   )
 }
 
