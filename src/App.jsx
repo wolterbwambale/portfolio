d97adc82b70bba9f52ffdd6ad786a8efcbeaf28a
Bwambale Benny Wolter <benplaan1@gmail.com>
2024-02-10 01:03:30 +0300

    add project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import Navbar from './components/Navbar/Navbar';
+import Project from "./components/Project/Project";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       < Hero/>
       <About />
       <Experience />
+      <Project/>
       </div>
      
   )
---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import projects from '../../data/project';
+import styles from './Project.module.css';
+import ProjectCard from './projectCard';
+
+
+const Project = () => {
+  return (
+    <section className={styles.container}>
+        <h1 className={styles.title}>Projects</h1>
+        <div className={styles.project}>
+            {projects.map((project, id) =>{ 
+                return < ProjectCard key={id} project={project} />
+            })}
+        </div>
+
+    </section>
+  )
+}
+
+export default Project
