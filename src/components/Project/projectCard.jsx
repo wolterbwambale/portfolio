b2b9d0011ce1d09fe7ef25fefa25ad5fb462c5ad
Bwambale Benny Wolter <benplaan1@gmail.com>
2024-02-13 23:44:41 +0300

    fix linter error

--- src/components/Project/projectCard.jsx
+++ src/components/Project/projectCard.jsx
@@ -1,26 +1,29 @@
-import styles from "./projectCard.module.css"
+/* eslint-disable */
+import styles from "./projectCard.module.css";
 import { getImageUrl } from '../../utils';
 
-const projectCard = ({project:{imagesrc,title,description,skills,demo,source}}) => {
+const ProjectCard = ({ project: { imagesrc, title, description, skills, demo, source } }) => {
   return (
     <div className={styles.container}>
       <img
         src={getImageUrl(imagesrc)}
-        alt={`image of ${title}` } className={styles.image}
+        alt={`image of ${title}`}
+        className={styles.image}
       />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}> 
-        {skills.map((skill) => {
-          return <li className={styles.skill}>{skill}</li>;
-        })}
+      <ul className={styles.skills}>
+        {skills.map((skill, index) => (
+          // Add a unique key prop to each li element
+          <li key={index} className={styles.skill}>{skill}</li>
+        ))}
       </ul>
       <div className={styles.links}>
         <a href={demo} className={styles.link}>Demo</a>
-        <a href={source} className={styles.link}>source</a>
+        <a href={source} className={styles.link}>Source</a>
       </div>
     </div>
   );
 };
 
-export default projectCard;
+export default ProjectCard;
