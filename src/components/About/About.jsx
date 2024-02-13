b8ef28ccdd858504006a8c5d35b272e7517ce24c
Bwambale Benny Wolter <benplaan1@gmail.com>
2024-02-13 21:01:55 +0300

    add update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <img src={getImageUrl("about/hacker.png")} alt="me seated with computer" className={styles.aboutMaainImg} />
             <ul className={styles.aboutItems}>
                 <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursor.png")} alt="pointer" className={styles.aboutImage} />
+                    <img src={getImageUrl("about/user-experience.png")} alt="pointer" className={styles.aboutImage} />
                 <div className={styles.aboutItemText}>
                 <h3>Frontend Developer</h3>
                 <p>I'm frontend with experience of building responsive and optimize sites</p>
@@ -24,7 +24,7 @@ const About = () => {
                 </li>
 
                 <li className={styles.aboutItem}>
-                    <img src={getImageUrl("about/cursor.png")} alt="pointer"className={styles.aboutImage} />
+                    <img src={getImageUrl("about/user-interface.png")} alt="pointer"className={styles.aboutImage} />
                 <div className={styles.aboutItemText}>
                 <h3>UI Design</h3>
                 <p>I have design multiple  landing pages and i have created  systems as well</p>
